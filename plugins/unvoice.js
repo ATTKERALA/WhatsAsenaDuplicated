218d1ce1ff119ea721ccbc8dbe42d31e641a25cc
ATTKERALA <78817825+ATTKERALA@users.noreply.github.com>
2021-03-01 23:39:59 -0800

    Update unvoice.js

--- plugins/unvoice.js
+++ plugins/unvoice.js
@@ -13,7 +13,7 @@ const cwebp = require('cwebp-bin');
 const Language = require('../language');
 const Lang = Language.getString('unvoice'); // Language support
 
-Asena.addCommand({pattern: 'unvoice', fromMe: true, desc: Lang.UV_DESC}, (async (message, match) => {    
+Asena.addCommand({pattern: 'unvoice', fromMe: false, desc: Lang.UV_DESC}, (async (message, match) => {    
     if (message.reply_message === false) return await message.sendMessage(Lang.UV_REPLY);
     var downloading = await message.client.sendMessage(message.jid,Lang.UV_PROC,MessageType.text);
     var location = await message.client.downloadAndSaveMediaMessage({
@@ -30,5 +30,5 @@ Asena.addCommand({pattern: 'unvoice', fromMe: true, desc: Lang.UV_DESC}, (async 
         .on('end', async () => {
             await message.sendMessage(fs.readFileSync('output.mp3'), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true});
         });
-    return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: true})
+    return await message.client.deleteMessage(message.jid, {id: downloading.key.id, remoteJid: message.jid, fromMe: false})
 }));
